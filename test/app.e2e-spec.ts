776b115180eca584c70c8ab2e67ca7cbeda57653
Clueroi <eriicsouzaromeroo@gmail.com>
2026-04-02 13:06:42 -0300

    Feat: VItest

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,8 +3,10 @@ import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
+import { afterEach, beforeEach, describe, it } from 'vitest';
 
-describe('AppController (e2e)', () => {
+describe
+('AppController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
