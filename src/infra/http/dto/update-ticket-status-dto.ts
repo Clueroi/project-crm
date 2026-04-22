3eea2f49a3438b535c7721838c1ddad7e3811771
Clueroi <eriicsouzaromeroo@gmail.com>
2026-04-22 12:22:26 -0300

    FEat: Forgotten files

---
+++ src/infra/http/dto/update-ticket-status-dto.ts
@@ -0,0 +1,13 @@
+import { IsString, IsOptional, IsIn } from "class-validator"
+
+export class UpdateTicketStatusDto {
+  @IsString()
+  @IsOptional()
+  @IsIn(["PENDING", "IN_PROGRESS", "RESOLVED", "CLOSED"])
+  status?: string
+
+  @IsString()
+  @IsOptional()
+  @IsIn(["LOW", "MEDIUM", "HIGH"])
+  priority?: string
+}
